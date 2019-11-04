aaf0eca0c51ce31961b4145a6163b8fe0a83a88b
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-11-04 09:44:15 -0500

    Coded the poem and asked for the month in numbers

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,21 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("There's old nursery rhyme that tells you what you're like depending on the day you were born.");
+        System.out.println("It goes:");
+        System.out.println("Monday's child is fair of face,");
+        System.out.println("Tuesday's child is full of grace,");
+        System.out.println("Wednesday's child is full of woe,");
+        System.out.println("Thursday's child has far to go.");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("but the child born  on the Sabbath Day,");
+        System.out.println("is fair and wise and good in every way.");
+
+        System.out.println("To find out exactly what weekday you were born on");
+        System.out.println("Enter the month you were born on in numbers");
+
+
+    }
+}
